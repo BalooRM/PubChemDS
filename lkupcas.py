e9d2c2a582dec842e6bd20ac2bb62c815b2c5d1a
BalooRM <richard_ballew@yahoo.com>
2019-05-09 10:56:12 -0400

    adding search by CAS

---
+++ lkupcas.py
@@ -0,0 +1,39 @@
+import pubchempy as pcp
+import sys
+import re
+
+usage = ("Usage: \n"
+         " > python lkupcas.py \"search term\"")
+         
+myargs = sys.argv
+if len(myargs) == 1:
+    sys.exit (usage)
+
+myname = myargs[1]  # ignore argv 0 = script name
+print (myname)
+    
+for compound in pcp.get_compounds(myname, 'name'):
+    myfound = 0
+    print ('CID\t', compound.cid)
+    print ('Formula\t', compound.molecular_formula)
+    print ('Mol Wt\t', compound.molecular_weight)
+    print ('Charge\t', compound.charge)
+    print ('XLogP\t', compound.xlogp)
+    print ('IUPAC Name\t', compound.iupac_name)
+    print ('InChI\t', compound.inchi)
+    print ('InChI Key\t', compound.inchikey)
+    print ('Fingerprint\t', compound.fingerprint)
+    print ('Isomeric SMILES\t', compound.isomeric_smiles)
+    print ('Canonical SMILES\t', compound.canonical_smiles)
+    for mysyn in compound.synonyms:
+        m = re.search('^CAS', mysyn)
+        if m != None:
+            myfound = 1
+            print ('CASRN\t', mysyn)
+        m = re.search('^UNII', mysyn)
+        if m != None:
+            print ('UNII\t', mysyn)
+    # if the CASRN was not found with the CAS prefix, use the search value
+    if myfound == 0:
+        print ('CASRN\t', myname)
+    print ('Synonyms:\n', compound.synonyms)
